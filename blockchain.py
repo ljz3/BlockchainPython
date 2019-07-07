39a08827966f99dbc03432bb3bffbd92ee54a33c
unknown <kevin.ottawa@hotmail.com>
2019-07-07 19:38:31 -0400

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -8,16 +8,41 @@ def add_value(transaction_amount, last_transaction = [1]):
     blockchain.append([last_transaction,transaction_amount])
     
 
-def get_user_input():
+def get_tx_value():
     return float(input("Your transaction amount please: "))
 
-tx_amount = get_user_input()
+def get_user_choice():
+	return input("Your choice: ")
+	
+
+def print_blockchain_elements():
+	for block in blockchain:
+		print("Outputting block")
+		print(block)
+
+
+tx_amount = get_tx_value()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(),transaction_amount= tx_amount)
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+while True:
+	print("Please choose")
+	print("1: Add a new transaction value")
+	print("2: Output the transaction blocks")
+	print("3: Quit")
+	
+	user_choice = get_user_choice()
+	
+	if user_choice == "1":
+		tx_amount = get_tx_value()
+		add_value(tx_amount, get_last_blockchain_value())
+	elif user_choice =="2":
+		print_blockchain_elements()
+	elif user_choice == "3":
+		break
+	else:
+		print("Input invalid, please pick from the list")
+	
+	
 
-print(blockchain)
+print("Done!")

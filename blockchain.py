41b8b3af98ca7e24b887c94c1ed2d417f004cf80
unknown <kevin.ottawa@hotmail.com>
2019-07-10 15:39:09 -0400

    Blockchain verification and manipulation added

--- blockchain.py
+++ blockchain.py
@@ -25,11 +25,28 @@ def print_blockchain_elements():
 		print(block)
 
 
+def verify_chain():
+	block_index = 0
+	is_valid = True
+	for block in blockchain:
+		if block_index==0:
+			block_index+=1
+			continue
+		elif block[0] == blockchain[block_index-1]:
+			is_valid = True
+		else:
+			is_valid = False
+			break
+		block_index+=1
+	return is_valid
+
+
 while True:
 	print("Please choose")
 	print("1: Add a new transaction value")
 	print("2: Output the transaction blocks")
-	print("3: Quit")
+	print("3: Manipulat the chain")
+	print("4: Quit")
 	
 	user_choice = get_user_choice()
 	
@@ -38,11 +55,19 @@ while True:
 		add_transaction(tx_amount, get_last_blockchain_value())
 	elif user_choice =="2":
 		print_blockchain_elements()
-	elif user_choice == "3":
+	elif user_choice =="3":
+		if len(blockchain)>=1:
+			blockchain[0] = [2]
+	elif user_choice == "4":
 		break
 	else:
 		print("Input invalid, please pick from the list")
-	
+
+	if verify_chain():
+		print("Valid!")
+	else:
+		print("Invalid!")
+		break
 	
 
 print("Done!")

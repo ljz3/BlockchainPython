e3c26bfc14d11edd9cb5678c3ad082a1ebaae197
ljz3 <kevin.ottawa@hotmail.com>
2019-07-31 02:19:37 -0400

    Using OrderedDict's instead of regular dictionaries for the consistent order of the key value pairs

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 from functools import reduce
 import hashlib as hl
 import json
+from collections import OrderedDict
 
 MINING_REWARD = 10
 
@@ -35,11 +36,13 @@ def add_transaction(recipient,sender = owner, amount = 1.0):
 		:recipient: The recipient of the coins
 		:amount: The amount of coins sent with the transaction (default = 1.0)
 	"""
-	transaction = {
-		"sender":sender,
-		"recipient":recipient, 
-		"amount": amount
-	}
+	# transaction = {
+	# 	"sender":sender,
+	# 	"recipient":recipient, 
+	# 	"amount": amount
+	# }
+
+	transaction = OrderedDict([("sender", sender), ("recipient", recipient), ("amount", amount)])
 
 	if verify_transaction(transaction):
 		open_transactions.append(transaction)
@@ -66,11 +69,14 @@ def mine_block():
 	last_block = blockchain[-1]
 	hashed_block = hash_block(last_block)
 	proof = proof_of_work()
-	reward_transaction = {
-		"sender": "MINING",
-		"recipient": owner,
-		"amount": MINING_REWARD
-	}
+	# reward_transaction = {
+	# 	"sender": "MINING",
+	# 	"recipient": owner,
+	# 	"amount": MINING_REWARD
+	# }
+
+	reward_transaction = OrderedDict([("sender", "MINING"), ("recipient", owner), ("amount", MINING_REWARD)])
+
 	copied_transactions = open_transactions[:]
 	copied_transactions.append(reward_transaction)
 	block = {
@@ -102,7 +108,7 @@ def print_blockchain_elements():
 
 
 def hash_block(block):
-	return hl.sha256(json.dumps(block).encode()).hexdigest()
+	return hl.sha256(json.dumps(block, sort_keys=True).encode()).hexdigest()
 
 
 

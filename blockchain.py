e81f60858bd15e6c2922dfeb5aee8f505128ccb7
ljz3 <kevin.ottawa@hotmail.com>
2019-07-31 02:07:27 -0400

    Added proof of work algorithm to verify the blockchain and mining the new block

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,5 @@
-import functools
-import hashlib
+from functools import reduce
+import hashlib as hl
 import json
 
 MINING_REWARD = 10
@@ -8,7 +8,8 @@ MINING_REWARD = 10
 GENESIS_BLOCK = {
 	"previous_hash": "", 
 	"index": 0, 
-	"transactions": []
+	"transactions": [],
+	"proof": 100
 	}
 blockchain = [GENESIS_BLOCK]
 open_transactions =[]
@@ -53,10 +54,10 @@ def get_balance(participant):
 	open_tx_sender = [tx["amount"] for tx in open_transactions if tx["sender"] == participant]
 	tx_sender.append(open_tx_sender)
 
-	amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
+	amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
 
 	tx_recipient = [[tx["amount"] for tx in block["transactions"] if tx["recipient"] == participant] for block in blockchain]
-	amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
+	amount_received = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
 
 	return amount_received - amount_sent
 
@@ -64,7 +65,7 @@ def get_balance(participant):
 def mine_block():
 	last_block = blockchain[-1]
 	hashed_block = hash_block(last_block)
-	print(hashed_block)
+	proof = proof_of_work()
 	reward_transaction = {
 		"sender": "MINING",
 		"recipient": owner,
@@ -75,7 +76,8 @@ def mine_block():
 	block = {
 		"previous_hash": hashed_block, 
 		"index": len(blockchain), 
-		"transactions": copied_transactions
+		"transactions": copied_transactions,
+		"proof": proof
 	}
 	blockchain.append(block)
 	return True
@@ -100,15 +102,34 @@ def print_blockchain_elements():
 
 
 def hash_block(block):
-	return hashlib.sha256(json.dumps(block).encode()).hexdigest()
+	return hl.sha256(json.dumps(block).encode()).hexdigest()
+
+
+
+def valid_proof(transactions, last_hash, proof):
+	guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+	guess_hash = hl.sha256(guess).hexdigest()
+	print(guess_hash)
+	return guess_hash[0:2] == "00"
+
+
+def proof_of_work():
+	last_block = blockchain[-1]
+	last_hash = hash_block(last_block)
+	proof = 0
+	while not valid_proof(open_transactions, last_hash, proof):
+		proof += 1
+	return proof
 
 
 def verify_chain():
 	for (index,block) in enumerate(blockchain):
 		if index == 0:
 			continue
-
 		if block["previous_hash"] != hash_block(blockchain[index-1]):
+			return False
+		if not valid_proof(block["transactions"][:-1], block["previous_hash"], block["proof"]):
+			print("POW is invalid")
 			return False
 	return True
 

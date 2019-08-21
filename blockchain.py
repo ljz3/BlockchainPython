10ce24c8488002f00b88550968a07a908042566a
ljz3 <kevin.ottawa@hotmail.com>
2019-08-21 00:37:15 -0400

    Update blockchain class to reflect changes in verification methods

--- blockchain.py
+++ blockchain.py
@@ -62,8 +62,7 @@ class Blockchain:
         last_block = self.chain[-1]
         last_hash = hash_block(last_block)
         proof = 0
-        verifier = Verification()
-        while not verifier.valid_proof(self.open_transactions, last_hash, proof):
+        while not Verification.valid_proof(self.open_transactions, last_hash, proof):
             proof += 1
         return proof
 
@@ -112,9 +111,8 @@ class Blockchain:
         #     'amount': amount
         # }
         transaction = Transaction(sender, recipient, amount)
-        verifier = Verification()
 
-        if verifier.verify_transaction(transaction, self.get_balance):
+        if Verification.verify_transaction(transaction, self.get_balance):
             self.open_transactions.append(transaction)
             self.save_data()
             return True
